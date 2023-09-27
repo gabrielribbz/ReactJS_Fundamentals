a481eaf962995f4b48db544640752cce540de3ee
gabrielribeiro <gabriel.ribeiro.fernandes00@gmail.com>
2023-09-27 16:16:37 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Post } from "./Post"
 
-function App() {
+export function App() {
 
   return (
-    <h1>hello world</h1>
+    <div>
+    <Post/>    
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+
+    </div>
   )
 }
 
-export default App
+
